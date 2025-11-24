c8484ea26f6b11230c02405b6210aa660f4305a3
Ngô Huỳnh Gia Thoại <thoai.ngo.dev@gmail.com>
2025-11-24 23:33:47 +0700

    fix bug

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useForm, SubmitHandler } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import provinceData from '../data/province.json'
 import wardData from '../data/ward.json'
 
